bfa0cec7a17274688399bfe699b66023cb9f5571
Omar Jbara <omar.jbara2@gmail.com>
2021-02-02 05:21:35 +0100

    - Added Typography story

---
+++ src/stories/Typography.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Meta, Story } from '@storybook/react'
+import { FONT_SIZES, KleeFontSize, KleeFontWeight } from '../styles/theme/typography'
+import { Heading, HStack, Text, VStack } from '../components'
+
+const meta: Meta = {
+  title: 'Tokens/Typography',
+  parameters: {
+    controls: { disable: true },
+  },
+}
+
+export default meta
+
+const SIZES = Object.entries(FONT_SIZES).map(([size, value]) => ({ size, value }))
+const WEIGHTS = Object.entries({
+  light: KleeFontWeight.Light,
+  normal: KleeFontWeight.Normal,
+  'semi bold': KleeFontWeight.Semibold,
+  bold: KleeFontWeight.Bold,
+  'extra bold': KleeFontWeight.Extrabold,
+}).map(([weight, value]) => ({ weight, value }))
+
+export const Default: Story = () => {
+  return (
+    <VStack spacing={4}>
+      <Heading as="h1">Nunito</Heading>
+      <VStack spacing={8}>
+        {SIZES.map((set, i) => (
+          <HStack align="center" spacing={4} key={`.0${i}`}>
+            <Text minWidth={12} color="cool-gray.400">
+              {set.size}
+            </Text>
+            <Text fontSize={set.value}>The quick brown fox jumps over the lazy dog 123456789.</Text>
+          </HStack>
+        ))}
+      </VStack>
+      <Heading as="h2">Weights</Heading>
+      {WEIGHTS.map((set, i) => (
+        <HStack align="center" spacing={4} key={`.0${i}`}>
+          <Text minWidth={16} color="cool-gray.400">
+            {set.weight}
+          </Text>
+          <Text fontSize={KleeFontSize.Xl4} fontWeight={set.value}>
+            The quick brown fox jumps over the lazy dog 123456789.
+          </Text>
+        </HStack>
+      ))}
+    </VStack>
+  )
+}
