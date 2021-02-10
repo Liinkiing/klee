7319172f38902248e96e2bd3f526046112490bca
Omar Jbara <omar.jbara2@gmail.com>
2021-02-10 02:34:44 +0100

    - More sizes for IconButton

--- src/components/button/IconButton.tsx
+++ src/components/button/IconButton.tsx
@@ -4,25 +4,35 @@ import styled from '@emotion/styled'
 import Button, { ButtonProps } from './Button'
 import { KleeFontSize } from '../../styles/theme/typography'
 
-export interface IconButtonProps extends Omit<ButtonProps, 'startIcon' | 'endIcon'> {
+type VariantSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl'
+export interface IconButtonProps extends Omit<ButtonProps, 'startIcon' | 'endIcon' | 'variantSize'> {
   icon: ReactElement
+  variantSize?: VariantSize
 }
 
 const IconButtonInner = styled(Button)(
   systemVariant<{}, NonNullable<IconButtonProps['variantSize']>>({
     prop: 'variantSize',
     variants: {
+      xs: {
+        p: 1,
+        fontSize: KleeFontSize.Xs,
+      },
       sm: {
         p: 1,
         fontSize: KleeFontSize.Sm,
       },
       md: {
         p: 2,
-        fontSize: KleeFontSize.Sm,
+        fontSize: KleeFontSize.Md,
       },
       lg: {
+        p: 3,
+        fontSize: KleeFontSize.Xl2,
+      },
+      xl: {
         p: 4,
-        fontSize: KleeFontSize.Lg,
+        fontSize: KleeFontSize.Xl5,
       },
     },
   }),
