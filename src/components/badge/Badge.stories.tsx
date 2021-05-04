5cb0a828ee97ad62664f8bb19d2b9d57d25ff198
Omar Jbara <omar.jbara2@gmail.com>
2021-05-04 06:58:35 +0200

    - Added Badge component

---
+++ src/components/badge/Badge.stories.tsx
@@ -0,0 +1,42 @@
+import { Meta, Story } from '@storybook/react'
+import React from 'react'
+
+import { useTheme } from '../../hooks/useTheme'
+import { HStack } from '../layout'
+import { Badge, BadgeProps } from './Badge'
+
+const meta: Meta<BadgeProps> = {
+  title: 'Library/Badge',
+  component: Badge,
+  parameters: {
+    controls: { expanded: true },
+  },
+}
+
+export default meta
+
+export const Default: Story<BadgeProps> = args => <Badge {...args} />
+
+Default.args = {
+  children: 'Hello Klee',
+}
+
+const EXCLUDED_COLORS = ['current', 'dark', 'black', 'white']
+
+export const AllColorScheme: Story<BadgeProps> = args => {
+  const theme = useTheme()
+  const colors = Object.keys(theme.colors).filter(color => !EXCLUDED_COLORS.includes(color))
+  return (
+    <HStack wrap="wrap" gap={4}>
+      {colors.map(color => (
+        <Badge key={color} {...args} colorScheme={color}>
+          {color}
+        </Badge>
+      ))}
+    </HStack>
+  )
+}
+
+AllColorScheme.argTypes = {
+  colorScheme: { table: { disable: true }, control: { disable: true } },
+}
--- src/components/types.ts
+++ src/components/types.ts
@@ -1,6 +1,15 @@
+import { KleeTheme } from '../styles/theme'
+
 export interface ShowableOnCreate {
   /**
    * When `true`, the component will be initially visible
    */
   readonly showOnCreate?: boolean
+}
+
+export interface ColorSchemable {
+  /**
+   * @default blue
+   */
+  readonly colorScheme?: keyof KleeTheme['colors'] | string
 }
